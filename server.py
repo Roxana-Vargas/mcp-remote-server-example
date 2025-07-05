9451290fe5fa2ee7c24f05ebf8908b2981cb7cc8
OLINDA ROXANA VARGAS CENTURION <U48493707@grupokonecta.pe>
2025-07-05 00:37:01 -0500

    add host

--- server.py
+++ server.py
@@ -7,6 +7,6 @@ def greet(name: str) -> str:
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http")
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000)
     print("Greetings tool is running...")
     print("Use the greet function to send greetings.")
